c033435f5d50a6d7ca3757339a71fa38f2e8040c
Hanz <hanzsteven2023@gmail.com>
2024-04-30 09:57:30 +0800

    adding web-info

---
+++ src/routes/+page.svelte
@@ -0,0 +1,153 @@
+<main class="w-full max-w-auto bg-slate-950 p-5 h-full">
+	<header class="text-white text-2xl flex p-4 font-mono">
+		<div>
+			<img
+				src="https://www.logomaker.com/api/main/images/1j+ojlxEOMkX9Wyqfhe43D6kh...GEpBVMmRjJwXs1M3EMoAJtlyEphvtt...PQ8PExevg9C3ktKMcs8"
+				alt="logo"
+				class="h-20 w-36"
+			/>
+		</div>
+		<ul class="flex flex-row ml-auto items-center space-x-3 text-xl">
+			<li>
+				<a
+					href="#"
+					class="hover:bg-teal-950 transition delay-150 duration-300 ease-in-out p-3 rounded-sm"
+					>Home</a
+				>
+			</li>
+			<li>
+				<a
+					href="#"
+					class="hover:bg-teal-950 transition delay-150 duration-300 ease-in-out p-3 rounded-sm"
+					>About Me</a
+				>
+			</li>
+			<li>
+				<a
+					href="#"
+					class="hover:bg-teal-950 transition delay-150 duration-300 ease-in-out p-3 rounded-sm"
+					>Projects</a
+				>
+			</li>
+			<li>
+				<a
+					href="#"
+					class="hover:bg-teal-950 transition delay-150 duration-300 ease-in-out p-3 rounded-sm"
+					>Contacts</a
+				>
+			</li>
+		</ul>
+	</header>
+	<section class="bg-teal-950 mt-32 rounded-sm">
+		<div class="grid grid-cols-2">
+			<div class="flex justify-center items-center mt-20">
+				<div class="text-2xl text-white font-bold space-y-5">
+					Hello, <span class="border bg-lime-200 text-black text-3xl rounded-md"> I'm Hanz!</span>
+					<div class="text-base text-white font-semibold">
+						~ I'm currently under maintainance, <br />sorry for inconvenience.
+					</div>
+					<div>
+						<h6
+							class="justify-center flex italic font-normal text-[10px] transition-opacity duration-500 hover:opacity-100 opacity-5"
+						>
+							"Scroll down to know more about me"
+						</h6>
+					</div>
+				</div>
+			</div>
+			<div class="flex justify-center p-5 mt-20">
+				<img
+					src="https://t4.ftcdn.net/jpg/07/93/31/35/240_F_793313518_PQ66p9W12GghNwSyRuTliwhdvxG6teUD.jpg"
+					alt="logo"
+					class="rounded-lg"
+				/>
+			</div>
+		</div>
+		<section>
+			<hr class="border-black border mt-32" />
+			<div class="flex justify-start p-5">
+				<div class="font-bold text-md text-slate-500"><li>ABOUT ME</li></div>
+			</div>
+			<div class="flex justify-center">
+				<div class="text-4xl text-gray-300 text-center">
+					𝗛𝗔𝗡𝗭𝗢
+					<div class="text-center text-[10px] text-gray-3000">STUDENT</div>
+					<div class="mx-52 my-10 mt-5">
+						<p class="text-left mt-5 font-normal text-lg">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem tempora at dicta
+							neque quod optio quos incidunt, fugiat autem odio, veritatis minus quibusdam porro
+							deserunt ut quae commodi dolor? Veritatis.
+						</p>
+						<p class="text-left mt-5 font-normal text-lg">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem tempora at dicta
+							neque quod optio quos incidunt, fugiat autem odio, veritatis minus quibusdam porro
+							deserunt ut quae commodi dolor? Veritatis.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="space-y-5">
+			<hr class="border-black border mt-32" />
+			<div class="flex justify-start p-5">
+				<div class="font-bold text-md text-slate-500"><li>PROJECTS</li></div>
+			</div>
+			<div class="flex justify-center">
+				<div class="text-4xl text-gray-300 text-center">MY WORKS</div>
+			</div>
+			<div class="grid grid-cols-2 px-10">
+				<div class="flex justify-center">
+					<img
+						src="https://t3.ftcdn.net/jpg/02/18/15/40/240_F_218154074_DtkUgqkC2SKy64I0NpC9qjNd3U6RJNtM.jpg"
+						alt="proj1"
+						class="rounded-md"
+					/>
+				</div>
+				<div class="flex justify-center">
+					<img
+						src="https://t4.ftcdn.net/jpg/02/01/39/13/240_F_201391364_bZ4dGb4ZmfYo21qfASvRfx3ywxevWCmy.webp"
+						alt="proj2"
+						class="rounded-md"
+					/>
+				</div>
+			</div>
+			<div class="grid grid-cols-2 px-10">
+				<div class="flex justify-center">
+					<img
+						src="https://t3.ftcdn.net/jpg/02/11/95/04/240_F_211950412_wsPP8grGn9fbJjSsp8WOs8pYSaariDSU.jpg"
+						alt="proj3"
+						class="rounded-md"
+					/>
+				</div>
+				<div class="flex justify-center">
+					<img
+						src="https://t3.ftcdn.net/jpg/02/06/29/24/240_F_206292429_R7I0NOzkyd0hRaPOwsgji3y8eknIulRC.jpg"
+						alt="proj4"
+						class="rounded-md"
+					/>
+				</div>
+			</div>
+		</section>
+	</section>
+	<footer class="text-white font-semibold text-xl mt-32 space-y-5">
+		<div class="flex justify-center">
+			<img
+				src="https://www.logomaker.com/api/main/images/1j+ojlxEOMkX9Wyqfhe43D6kh...GEpBVMmRjJwXs1M3EMoAJtlyEphvtt...PQ8PExevg9C3ktKMcs8"
+				alt="logo"
+				class="h-auto w-auto"
+			/>
+		</div>
+		<div class="flex justify-center space-x-5">
+			<div class="text-[15px] font-gray-300 text-left font-normal hover:underline">
+				<a href="#">🌐 www.hanzo.com.ph</a>
+			</div>
+			<div class="text-[15px] font-gray-300 text-left font-normal hover:underline">
+				<a href="#">📧 hanzo@gmail.com</a>
+			</div>
+			<div class="text-[15px] font-gray-300 text-left font-normal">🖁 +639 075 049 974</div>
+			<div class="text-[15px] font-gray-300 text-left font-normal">
+				📌 #152 Panilao, Pilar, Bataan
+			</div>
+		</div>
+	</footer>
+</main>
